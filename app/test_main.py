3d2edc1401b6f9235cf9a577a92cbc2b96a894c8
Atom-gnomov <gorbach0864213579@gmail.com>
2025-01-20 11:26:55 +0200

    complete

--- app/test_main.py
+++ app/test_main.py
@@ -5,14 +5,13 @@ import pytest
 @pytest.mark.parametrize(
     "coins, expected",
     [
-        (7 ,[2, 1, 0, 0]),
+        (7, [2, 1, 0, 0]),
         (0, [0, 0, 0, 0]),
         (99, [4, 0, 2, 3]),
         (100, [0, 0, 0, 4]),
         (88, [3, 0, 1, 3])
     ]
 )
+def test_coins(coins: int , expected: list) -> None:
 
-def test_coins(coins, expected):
     assert get_coin_combination(coins) == expected
-
